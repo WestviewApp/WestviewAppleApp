9356684520f675af055235a1b12a16c5c6506bc9
Ronak Shah <ronakshahultimate@gmail.com>
2017-08-02 19:22:50 -0700

    Initial Commit

---
+++ Westview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Westview
+//
+//  Created by Ronak Shah on 8/2/17.
+//  Copyright © 2017 Ronak Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
