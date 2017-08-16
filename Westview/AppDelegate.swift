9a6769699cdd3fcbce30fd62ecf158b534224c88
Ronak Shah <ronakshahultimate@gmail.com>
2017-08-15 23:56:25 -0700

    Added Google Maps API, will implement later

--- Westview/AppDelegate.swift
+++ Westview/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GooglePlaces
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UIApplication.shared.statusBarStyle = .lightContent
+        GMSPlacesClient.provideAPIKey("AIzaSyBGxpDbkNT_xwngKaNEpbNvG-kSYhQyZuM")
+        GMSServices.provideAPIKey("AIzaSyBGxpDbkNT_xwngKaNEpbNvG-kSYhQyZuM")
         return true
     }
 
